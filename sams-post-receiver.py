4526436fbc18d573d0c182cb7e476742e65bd266
Hugo U. R. Strand <hugo.strand@gmail.com>
2019-11-13 21:52:58 +0100

    [thsv] sams-post-reciever.py

--- sams-post-receiver.py
+++ sams-post-receiver.py
@@ -19,8 +19,6 @@ import sams.core
 import logging
 logger = logging.getLogger(__name__)
 
-id = 'sams.post-receiver'
-
 class Receiver(MethodView):
     def __init__(self,base_path,jobid_hash_size):
         super(Receiver,self).__init__()
@@ -37,7 +35,7 @@ class Receiver(MethodView):
             if not os.path.isdir(base_path):
                 try:
                     os.mkdir(base_path)
-                except Exception as err:
+                except IOError as err:
                     # Handle possible raise from other process
                     if not os.path.isdir(base_path):
                         assert False, "Failed to mkdir '%s' " % base_path
@@ -47,11 +45,11 @@ class Receiver(MethodView):
             with open(os.path.join(base_path,tfilename),"wb") as file:
                 file.write(request.data)
             os.rename(os.path.join(base_path,tfilename),os.path.join(base_path,filename))
-        except Exception as err:
+        except IOError as err:            
             logger.debug("Failed to write file")
             try:
                 os.unlink(os.path.join(base_path,tfilename))
-            except Exception as err:
+            except IOError as err:
                 # Just log unlink errors
                 logger.error("Failed to unlink tmp file")                
                 pass
@@ -95,18 +93,14 @@ class Main:
         # Logging
         loglevel = self.options.loglevel
         if not loglevel:
-            loglevel = self.config.get([id,'loglevel'],'ERROR')
-        if not loglevel:
-            loglevel = self.config.get(['common','loglevel'],'ERROR')
+            loglevel = self.config.get(['core','loglevel'],'ERROR')
         loglevel_n = getattr(logging, loglevel.upper(), None)
         if not isinstance(loglevel_n, int):
             raise ValueError('Invalid log level: %s' % loglevel)
         logfile = self.options.logfile
         if not logfile:
-            logfile = self.config.get([id,'logfile'])
-        if not logfile:
-            logfile = self.config.get(['common','logfile'])
-        logformat = self.config.get([id,'logformat'],'%(asctime)s %(name)s:%(levelname)s %(message)s')
+            logfile = self.config.get(['core','logfile'])
+        logformat = self.config.get(['core','logformat'],'%(asctime)s %(name)s:%(levelname)s %(message)s')
         if logfile:
             logging.basicConfig(filename=logfile, filemode='a',
                                 format=logformat,level=loglevel_n)
@@ -116,11 +110,11 @@ class Main:
     def start(self):
         app = Flask(__name__)
         view_func = Receiver.as_view('receiver', 
-                                base_path=self.config.get([id,'base_path'],'/tmp'),
-                                jobid_hash_size=self.config.get([id,'jobid_hash_size'])
+                                base_path=self.config.get(['core','base_path'],'/tmp'),
+                                jobid_hash_size=self.config.get(['core','jobid_hash_size'])
                              )
         app.add_url_rule('/<int:jobid>/<filename>',view_func=view_func)
-        app.run(host=self.config.get([id,'bind'],'127.0.0.1'), port=self.config.get([id,'port'],8080) )
+        app.run(host=self.config.get(['core','bind'],'127.0.0.1'), port=self.config.get(['core','port'],8080) )
 
 if __name__ == "__main__":
     Main().start()
